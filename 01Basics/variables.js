773a7bfcb340e2a9432498d6682c559b8e5525a9
deep_ramani <41836849+deepramani8505@users.noreply.github.com>
2021-04-10 19:33:38 +0530

    VariableEx

---
+++ 01Basics/variables.js
@@ -0,0 +1,20 @@
+let name='deep'
+name='DEEP'
+let surname='ramani'
+
+let age=21
+
+console.log('hello')
+console.log(name)
+console.log(surname)
+console.log(age)
+
+let fullname=name + surname + age
+console.log(fullname)
+
+console.log(name+' '+surname+' '+age)
+
+// How to change the variable value 
+
+// for example let name='deep' we have to change the value deep then simply write variable name ane value don't write let keyword
+
