12f62a3599028f42b38c13cf39ef95d2aab1e1b2
deep_ramani <41836849+deepramani8505@users.noreply.github.com>
2021-04-10 19:32:52 +0530

    celsuis file

---
+++ 01Basics/celsuis.js
@@ -0,0 +1,19 @@
+var score=130
+const bouns=50
+
+var finalScore=score + bouns
+
+console.log(finalScore)
+
+var complex= (4 + 4) * 10.5
+console.log(complex)
+
+// find temprature in fahrenheit
+
+var tempFahrenhit = 20  
+
+// formula to fahrenheit to celcuis convert 
+// T(°C) = (T(°F) - 32) × 5/9
+
+var celcuis=(tempFahrenhit-32) *5/9
+console.log(celcuis)
